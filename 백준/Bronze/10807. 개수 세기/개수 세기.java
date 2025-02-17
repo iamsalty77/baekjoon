96a4218b6188440d4b5626c8e71ee00c7d5a5e80
JYK <iamsalty77@gmail.com>
2025-02-17 22:21:14 +0900

    [Bronze V] Title: 개수 세기, Time: 180 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int v = sc.nextInt();
+
+        int count = 0;
+        for(int number : numbers) {
+            if(number == v) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
