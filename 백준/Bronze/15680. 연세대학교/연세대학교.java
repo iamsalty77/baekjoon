05db8e981d448756eab706bc6fd0713790c40946
JYK <iamsalty77@gmail.com>
2025-02-19 21:22:05 +0900

    [Bronze V] Title: 연세대학교, Time: 168 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n == 1) {
+            System.out.println("Leading the Way to the Future");
+        }else {
+            System.out.println("YONSEI");
+        }
+    }
+}
