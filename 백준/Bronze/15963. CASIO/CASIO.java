149e58312d8f23a5c93396be7a47118cda83b1d6
JYK <iamsalty77@gmail.com>
2025-02-21 23:13:39 +0900

    [Bronze V] Title: CASIO, Time: 168 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/15963. CASIO/CASIO.java
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -1,13 +1,16 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        long n = scanner.nextLong();
-        long m = scanner.nextLong();
 
-        if (n == m) {
+        BigInteger n = scanner.nextBigInteger();
+        BigInteger m = scanner.nextBigInteger();
+
+
+        if (n.equals(m)) {
             System.out.println(1);
         } else {
             System.out.println(0);
