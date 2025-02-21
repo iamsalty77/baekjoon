09d380128252080781146d7777ee92381dbc745a
JYK <iamsalty77@gmail.com>
2025-02-21 23:03:42 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 176 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt();
+        int t = (int)Math.ceil((double)L/5);
+        System.out.println(t);
+        }
+    }
