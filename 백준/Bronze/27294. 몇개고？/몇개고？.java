08b85d576294365171bb7b6c4accaa72a976a80f
JYK <iamsalty77@gmail.com>
2025-02-25 21:54:40 +0900

    [Bronze V] Title: 몇개고?, Time: 176 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int s = sc.nextInt();
+        if(t >= 12 && t <= 16) {
+            if(s == 0) {
+                System.out.println(320);
+            }else {
+                System.out.println(280);
+            }
+        }else {
+            System.out.println(280);
+        }
+    }
+}
