ea9372ff47fa08adab7a52a1a83e8d524aa75427
JYK <iamsalty77@gmail.com>
2025-02-25 23:34:57 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 164 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String abbreviation = sc.next();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("NLCS", "North London Collegiate School");
+        map.put("BHA", "Branksome Hall Asia");
+        map.put("KIS", "Korea International School");
+        map.put("SJA", "St. Johnsbury Academy");
+
+        System.out.println(map.get(abbreviation));
+    }
+}
